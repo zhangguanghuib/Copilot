36a3e890aef47c6f5f2bd1ca72d0ddacb5c4ae15
zhangguanghuib <=>
2024-05-12 15:12:37 +0800

    fix a redux bug

--- React18_Heima/Redux_React_Proj01/src/main.jsx
+++ React18_Heima/Redux_React_Proj01/src/main.jsx
@@ -9,8 +9,8 @@ import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider>
-         <App store={store} />
+    <Provider store={store}>
+         <App />
     </Provider>
   </React.StrictMode>,
 )
